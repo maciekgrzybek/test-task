6887a00a8baf362ed46ff8b80980c78cf87b18e8
maciek_grzybek <maciekgrzybek1@gmail.com>
2018-07-22 17:36:42 +0100

    second

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import "./index.scss";
+
 // prettier-ignore
 const initialArray = [3,7,'d','3',7,8,'df',,23,6,2,67,'g','34',12,34,5];
 
 const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
 const filteredArray = initialArray.filter(isNumeric);
 
-const calculateAverage = arr => {
-  const length = arr.length;
-  let sum = arr.reduce((a, b) => {
-    return parseFloat(a) + parseFloat(b);
-  });
-  return sum / length;
-};
-
-const showBox = arr => {
+const displayBox = arr => {
   arr.map(item => {
     let newEl = document.createElement("div");
     newEl.innerHTML = item;
@@ -25,13 +18,27 @@ const showBox = arr => {
   });
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-  showBox(filteredArray);
-  const button = document.getElementById("js-calculate-average");
+const calculateAverage = arr => {
+  const length = arr.length;
+  let sum = arr.reduce((a, b) => {
+    return parseFloat(a) + parseFloat(b);
+  });
+
+  return (sum / length).toFixed(7);
+};
+
+const showResult = result => {
   const container = document.getElementById("js-result-container");
+  container.innerHTML = result;
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  displayBox(filteredArray);
+
+  const button = document.getElementById("js-calculate-average");
 
   button.addEventListener("click", () => {
     const result = calculateAverage(filteredArray);
-    container.innerHTML = result;
+    showResult(result);
   });
 });
